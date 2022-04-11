98cad70fde24d1ac41cecb2b1e0c9da48810a507
gandong <gandong@37.com>
2022-04-11 15:26:02 +0800

    活动功能

---
+++ 04-11-day01/src/pers/gandong/git/Activity.java
@@ -0,0 +1,18 @@
+package pers.gandong.git;
+
+/**
+ * @description: 新增活动
+ * @author: gandong
+ * @date: 2022-04-11 15:24
+ */
+public class Activity {
+
+    /**
+     * @param times 次数
+     * @return boolean
+     * @throws
+     */
+    public boolean draw(int times) {
+        return true;
+    }
+}
