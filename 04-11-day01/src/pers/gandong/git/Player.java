a07e94a9217c4a14acc5a508453f9cc028fe7256
gandong <gandong@37.com>
2022-04-11 14:17:48 +0800

    添加Player

---
+++ 04-11-day01/src/pers/gandong/git/Player.java
@@ -0,0 +1,28 @@
+package pers.gandong.git;
+
+/**
+ * @description: 玩家类
+ * @author: gandong
+ * @date: 2022-04-11 11:54
+ */
+public class Player {
+
+    /**
+     * 玩家ID
+     */
+    private String id;
+    /**
+     * 名称
+     */
+    private String name;
+
+
+    /**
+    * @param
+    * @return
+    * @throws
+    */
+    public void sayHello(){
+        System.out.println("你好，我是gandong");
+    }
+}
